c72c56618e1c57ff85565e92436f24d1fa7f5f85
maucon <maucon13@gmail.com>
2020-12-21 20:53:37 +0100

    Changed small primes loading

--- src/main.py
+++ src/main.py
@@ -3,16 +3,16 @@ from time import time
 
 import numpy as np
 
+from file_compressor import read_compressed_prime_file
 from prime_generator import calculate_sieve, get_primes
 
 loading_primes_time = time()
 blocks, block_size = 256, 64
 offset = 1000000000
 print('Calculating start point...')
-start = max(map(lambda path: int(path.split('-')[1].split('.')[0]), glob('data/*.txt')))
+start = max(map(lambda path: int(path.split('-')[1].split('.')[0]), glob('../data/*.*prime')))
 print('Loading primes...')
-with open('data/0-1000000000.txt', 'r') as file:
-    small_primes = np.array(list(map(int, file.readlines()[1:])))
+small_primes = read_compressed_prime_file('../data/0-1000000000', offset)[1:]
 print('INIT VALUES:', time() - loading_primes_time, end='\n\n')
 
 for calc_round in range(1, 101):
@@ -29,9 +29,9 @@ for calc_round in range(1, 101):
     print('READ PRIMES:', time() - part_time)
 
     part_time = time()
-    with open('data/%s-%s.txt' % (start, start + offset), 'w+') as file:
+    with open('../data/%s-%s.prime' % (start, start + offset), 'w+') as file:
         file.write('\n'.join(map(str, final_primes)))  # maybe chunk it to reduce ram spikes
-    print('SAVING TXT :', time() - part_time)
+    print('SAVING FILE:', time() - part_time)
 
     print('--------------------------------\nROUND TIME : %s' % (time() - start_time), end='\n\n')
     start += offset
