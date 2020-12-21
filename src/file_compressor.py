8d9ccb5aa3c48bab14184125eb888c0a6dbe7264
maucon <maucon13@gmail.com>
2020-12-21 23:43:26 +0100

    Working prime compress process

--- src/file_compressor.py
+++ src/file_compressor.py
@@ -3,12 +3,12 @@ from os import remove
 import numpy as np
 
 
-def compress_prime_file(filepath, step_size, calculation_offset):
+def compress_prime_file(filepath, step_size, offset):  # fixme
     with open(filepath + '.prime', 'r') as file:
         primes = np.array(list(map(int, file.readlines())))
 
     grouped, index, step = [], 0, 1
-    while step * step_size <= calculation_offset:
+    while step * step_size <= offset:
         last_prime, stepper, group = 0, step * step_size, []
         while index < len(primes) and primes[index] < stepper:
             group.append(str(primes[index] - (step - 1) * step_size if len(group) == 0
@@ -37,3 +37,24 @@ def read_compressed_prime_file(filepath, step_size):
                 primes.append(primes[-1] + inc)
             line_count += 1
     return np.array(primes)
+
+
+def save_and_compress_prime_file(start, offset, step_size, primes):
+    grouped, index, step = [], 0, 1
+    while step * step_size <= offset:
+        last_prime, stepper, group = 0, start + step * step_size, []
+        while index < len(primes) and primes[index] < stepper:
+            group.append(str(primes[index] - (start + (step - 1) * step_size) if len(group) == 0
+                             else (primes[index] - last_prime) // 2))
+            last_prime = primes[index]
+            index += 1
+        grouped.append(' '.join(group))
+        step += 1
+
+    with open('../data/%s-%s.cprime' % (start, start + offset), 'w+') as file:
+        file.write('\n'.join(grouped))
+
+
+def save_prime_file(start, offset, primes):
+    with open('../data/%s-%s.prime' % (start, start + offset), 'w+') as file:
+        file.write('\n'.join(map(str, primes)))
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from time import time
 
 import numpy as np
 
-from file_compressor import read_compressed_prime_file
+from file_compressor import read_compressed_prime_file, save_and_compress_prime_file
 from prime_generator import calculate_sieve, get_primes
 
 loading_primes_time = time()
@@ -30,8 +30,8 @@ for calc_round in range(1, 101):
     print('READ PRIMES:', time() - part_time)
 
     part_time = time()
-    with open('../data/%s-%s.prime' % (start, start + offset), 'w+') as file:
-        file.write('\n'.join(map(str, final_primes)))  # maybe chunk it to reduce ram spikes
+    # save_prime_file(start, offset, final_primes)
+    save_and_compress_prime_file(start, offset, step_size, final_primes)
     print('SAVING FILE:', time() - part_time)
 
     print('--------------------------------\nROUND TIME : %s' % (time() - start_time), end='\n\n')
